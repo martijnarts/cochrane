1392f973a654539fea80048cd00c5a245677a342
Martijn Arts <martijn@martijnarts.com>
2023-10-13 15:23:01 +0200

    Try some ways to print the tfplan

--- infra/main.tf
+++ infra/main.tf
@@ -1,26 +1,26 @@
 variable "github_token" {
-  type = string
+  type        = string
   description = "Your Github OAuth token"
 }
 
 variable "github_owner" {
-  type = string
+  type        = string
   description = "The owner of the Github repository"
 }
 
 variable "github_name" {
-  type = string
+  type        = string
   description = "The name of the Github repository"
 }
 
 variable "github_visibility" {
-  type = string
-  default = "private"
+  type        = string
+  default     = "private"
   description = "The visibility of the Github repository (public or private)"
 }
 
 variable "gha_op_service_account" {
-  type = string
+  type        = string
   description = "A 1Password service account token with access to the right vault"
 }
 
@@ -39,19 +39,19 @@ provider "github" {
 }
 
 resource "github_repository" "repository" {
-  name = var.github_name
+  name       = var.github_name
   visibility = var.github_visibility
 
-  has_issues = true
-  has_projects = false
-  has_wiki = false
-  has_downloads = false
+  has_issues           = true
+  has_projects         = false
+  has_wiki             = false
+  has_downloads        = false
   vulnerability_alerts = true
 }
 
 resource "github_actions_secret" "op_service_account" {
   repository = var.github_name
 
-  secret_name = "OP_SERVICE_ACCOUNT"
+  secret_name     = "OP_SERVICE_ACCOUNT"
   plaintext_value = var.gha_op_service_account
 }
