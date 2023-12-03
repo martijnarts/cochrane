1228758abdbf21c96d3a5e3714e544b79579a7b5
Martijn Arts <martijn@martijnarts.com>
2023-12-03 01:15:27 +0100

    Fix deployment

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,20 +1,21 @@
 use dioxus::prelude::{dioxus_elements, rsx, use_state, Element, Scope};
 use dioxus_fullstack::prelude::LaunchBuilder;
-use tracing_subscriber;
 
-#[cfg(ssr)]
+#[cfg(feature = "ssr")]
 fn main() {
+    use tracing_subscriber;
     tracing_subscriber::fmt::init();
 
     LaunchBuilder::new(app)
-        .addr([[0, 0, 0, 0], 8080].into())
+        .addr(std::net::SocketAddrV4::new(
+            std::net::Ipv4Addr::new(0, 0, 0, 0),
+            8080,
+        ))
         .launch();
 }
 
-#[cfg(not(ssr))]
+#[cfg(not(feature = "ssr"))]
 fn main() {
-    tracing_subscriber::fmt::init();
-
     LaunchBuilder::new(app).launch();
 }
 
